f9f7149598c25844b842981ae96dba77c71c7447
oBR1o <owen55660@gmail.com>
2023-03-06 16:00:37 +0700

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import Axios from "axios";
+import Cookies from "universal-cookie";
 
-function Login() {
+function Login({ setIsAuth }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = ( ) =>{};
+  const cookies = new Cookies();
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username,
+      password,
+    }).then((res) => {
+      const { firstName, lastName, username, token, userId } = res.data;
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("username", username);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      setIsAuth(true);
+    });
+  };
   return (
     <div className="login">
       <label> Login</label>
@@ -24,7 +40,8 @@ function Login() {
       />
       <button onClick={login}> Login</button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
+
